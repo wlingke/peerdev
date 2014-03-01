98a8b6eaac5f1e7d2467f2263e6cc40fb76fe93f
Lingke <wlingke@gmail.com>
2014-02-28 23:49:43 -0500

    creating a project works

--- app/projects/projectHandlers.js
+++ app/projects/projectHandlers.js
@@ -3,3 +3,23 @@
 var mongoose = require('mongoose'),
     Project = mongoose.model('Project'),
     _ = require('lodash');
+
+
+module.exports.create = function (req, res, next) {
+    var newProject = {
+        data: {},
+        relations: {}
+    };
+    _.assign(newProject.data, req.body);
+    newProject.relations.owner = req.session.profile._id;
+
+    var project = new Project(newProject);
+    project.save(function (err) {
+        if (err) {
+            res.send(400, err);
+            return;
+        }
+        res.send(201);
+    })
+};
+
--- app/projects/projectsAPI.js
+++ app/projects/projectsAPI.js
@@ -1,10 +1,14 @@
 var projectHandlers = require('./projectHandlers');
+var userHandlers = require('app/users/userHandlers');
 
 'use strict';
 
 module.exports = function (app) {
 
-    app.post('/api/projects', function(req,res,next){res.send('hello world')});
+    app.post('/api/projects', userHandlers.loggedIn, projectHandlers.create);
     app.get('/api/projects/:id');
+
+    app.post('/api/projects/:id');
+    app.del('/api/projects/:id');
 
 };
